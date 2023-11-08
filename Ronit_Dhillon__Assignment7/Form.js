2e35a7b7270da58e8ae054677401bc73fd281676
Ronit-dh <133061914+Ronit-dh@users.noreply.github.com>
2023-11-07 23:50:05 -0500

    Add files via upload

---
+++ Ronit_Dhillon__Assignment7/Form.js
@@ -0,0 +1,42 @@
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  $(document).ready(function () {
+    $("#myForm").validate(
+        {
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 6
+            },
+            phone: {
+                required: true,
+                pattern: /^\d{3}-\d{3}-\d{4}$/
+            }
+        },
+        messages: {
+            name: "Please enter your name",
+            email: {
+                required: "Please enter your email",
+                email: "Please enter a valid email address"
+            },
+            password: {
+                required: "Please enter a password",
+                minlength: "Password must be at least 6 characters long"
+            },
+            phone: {
+                required: "Please enter a phone number",
+                pattern: "Please enter a valid US phone number (e.g., 123-456-7890)"
+            }
+        }
+    });
+});
